2d143ef7b214d3efffb57a50a98925176fd67dfe
Larchenzio <107085516+Larchenzio@users.noreply.github.com>
2024-07-12 15:50:13 -0300

    Add files via upload

---
+++ Unidad 2/Desafios/Desafio_Repeticiones.py
@@ -0,0 +1,9 @@
+'''Bienvenido a los desafíos
+Enunciado:
+Crear un programa que muestre en la consola 
+10 repeticiones con números ASCENDENTE desde el 1 al 10'''
+
+contador=1
+while contador <=10:
+    print(contador)
+    contador = contador + 1
