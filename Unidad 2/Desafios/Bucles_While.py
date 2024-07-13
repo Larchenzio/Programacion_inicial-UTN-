301dfaabc56ec568bdc4206224f669bc5fbea58c
Larchenzio <107085516+Larchenzio@users.noreply.github.com>
2024-07-13 01:38:18 -0300

    Delete Unidad 2/Desafios/Bucles_While.py

--- Unidad 2/Desafios/Bucles_While.py
+++
@@ -1,9 +0,0 @@
-'''Bienvenido a los desafíos
-Enunciado:
-Crear un programa que muestre en la consola 
-10 repeticiones con números ASCENDENTE desde el 1 al 10'''
-
-contador=1
-while contador <=10:
-    print("El contador es:",contador)
-    contador += 1
